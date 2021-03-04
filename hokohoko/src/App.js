4094b837a3b11f5d74e2e1e30a71f9da84fda487
rielleriri <rielleriri@gmail.com>
2021-03-04 17:41:07 +0800

    try 1

--- hokohoko/src/App.js
+++ hokohoko/src/App.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 
 var Tesseract = window.Tesseract;
+
+const NavBar = () => (
+  <div className="navbar">
+    <h3>Task Manager</h3>
+    <Link to="/">Current Tasks</Link>
+    <Link to="/completed">Completed Tasks</Link>
+  </div>
+);
+
+const Template = (props) => (
+  <div>
+    <NavBar />
+    <p className="page-info">
+      {props.title}:
+    </p>
+    <ul className={props.status}>
+        <li>Task 1</li>
+        <li>Task 2</li>
+        <li>Task 3</li>
+    </ul>
+  </div>
+);
+
+const CurrentTasks = () => (
+  <Template title="Current Tasks" status="Current"/>
+);
+
+const CompletedTasks = () => (
+  <Template title="Completed Tasks" status="Completed"/>
+);
 
 class App extends Component {
 
@@ -70,6 +101,14 @@ class App extends Component {
         <header className="header">
           <h1>My OCR App</h1>
         </header>
+
+        {/* Navbar */}
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={CurrentTasks}/>
+            <Route path="/completed" component={CompletedTasks}/>
+          </div>
+        </BrowserRouter>
 
         { /* File uploader */ }
         <section className="hero">
