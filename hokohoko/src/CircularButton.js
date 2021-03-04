f4c5d08ebd0a7fe8626fd75f5f6894fcd1fcc6b5
Hartanto Ario Widjaya <tanto259@users.noreply.github.com>
2021-03-04 16:28:39 +0800

    Add color to small button

--- hokohoko/src/CircularButton.js
+++ hokohoko/src/CircularButton.js
@@ -19,6 +19,9 @@ export default class CircularButton extends React.Component {
             left: this.state.left,
             top: this.state.top,
             filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
+            background: "#19B09F",
+            border: "1px solid #FFFFFF",
+            boxSizing: "border-box",
         };
 
         return (
