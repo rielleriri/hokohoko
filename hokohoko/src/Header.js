9f1db493cbd96f3def25e0551107e1d3b77fd11a
JasmineIsHere <61452236+JasmineIsHere@users.noreply.github.com>
2021-03-04 17:41:09 +0800

    header

--- hokohoko/src/Header.js
+++ hokohoko/src/Header.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import "./Header.css";
 
+//jasmine
 function Header() {
     return (
-        <div>
-            
+        <div className = "header">
+            <div className = "header__back">
+                <a>Back</a>
+            </div>
+            <div className = "header__name">
+                <span className = "header__line">Results</span>
+            </div>
         </div>
     )
 }
