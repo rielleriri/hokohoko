f618a3206f4f83575b1f4e2b3f69dc56e15adf96
Hartanto Ario Widjaya <tanto259@users.noreply.github.com>
2021-03-04 16:18:46 +0800

    First try in creating a button component

--- hokohoko/src/Button.js
+++ hokohoko/src/Button.js
@@ -1,11 +1,39 @@
 import React from 'react'
 
-function Button() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default class Button extends React.Component {
+    constructor(props) {
+        this.state = {
+            text: props.text,
+            left: props.left,
+            top: props.top
+        }
+    }
+
+    render() {
+
+        var text = this.state.text;
+
+        const mystyle = {
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            padding: "16px",
+            position: "absolute",
+            width: "133px",
+            height: "54px",
+            left: this.state.left,
+            top: this.state.top,
+            background: "#19B09F",
+            boxShadow: "0px 10px 30px rgba(64, 191, 255, 0.24)",
+            borderRadius: "50px",
+        };
+
+        return (
+            <button style={style}>
+                {text}
+            </button>
+        );
+    }
 }
 
-export default Button
+
