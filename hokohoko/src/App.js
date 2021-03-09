844cd06e0457a48db5a24e526f69fde27248a089
meganseah <49828224+meganseah@users.noreply.github.com>
2021-03-09 15:37:48 +0800

    Integrate with OCR

--- hokohoko/src/App.js
+++ hokohoko/src/App.js
@@ -1,119 +1,69 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState, Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { createWorker } from 'tesseract.js';
 import './App.css';
 
-var Tesseract = window.Tesseract;
+const NavBar = () => (
+  <div className="navbar">
+    <h3>HokoHoko</h3>
+    <Link to="/">Lists</Link>
+    <Link to="/new">New Comparison</Link>
+  </div>
+);
+
+const Template = (props) => (
+  <div>
+    <NavBar />
+    <p className="page-info">
+      {props.title}:
+    </p>
+    <ul className={props.status}>
+        <li>Task 1</li>
+        <li>Task 2</li>
+        <li>Task 3</li>
+    </ul>
+    {PerformOCR("http://localhost:3000/lazada.jpg")}
+  </div>
+);
+
+const ComparisonList = () => (
+  <Template title="Lists" status="Lists"/>
+);
+
+const AddNewComparison = () => (
+  <Template title="Add Comparison" status="Add"/>
+);
 
 class App extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      uploads: [],
-      patterns: [],
-      documents: []
-    };
-  }
-  handleChange = (event) => {
-    if (event.target.files[0]) {
-      var uploads = []
-      for (var key in event.target.files) {
-        if (!event.target.files.hasOwnProperty(key)) continue;
-        let upload = event.target.files[key]
-        uploads.push(URL.createObjectURL(upload))
-      }
-      this.setState({
-        uploads: uploads
-      })
-    } else {
-      this.setState({
-        uploads: []
-      })
-    }
-  }
-
-  generateText = () => {
-    let uploads = this.state.uploads
-  
-    for(var i = 0; i < uploads.length; i++) {
-      Tesseract.recognize(uploads[i], 'eng')
-      .catch(err => {
-        console.error(err)
-      })
-      .then(result => {
-        // Get Confidence score
-        let confidence = result.confidence
-  
-        // Get full output
-        let text = result.text
-  
-        // Get codes
-        let pattern = /\b\w{10,10}\b/g
-        let patterns = result.text.match(pattern);
-  
-        // Update state
-        this.setState({ 
-          patterns: this.state.patterns.concat(patterns),
-          documents: this.state.documents.concat({
-            pattern: patterns,
-            text: text,
-            confidence: confidence
-          })
-        })
-      })
-    }
-  }
-  
-
   render() {
     return (
-      <div className="app">
-        <header className="header">
-          <h1>My OCR App</h1>
-        </header>
-
-        { /* File uploader */ }
-        <section className="hero">
-          <label className="fileUploaderContainer">
-            Click here to upload documents
-            <input type="file" id="fileUploader" onChange={this.handleChange} multiple />
-          </label>
-
-          <div>
-            { this.state.uploads.map((value, index) => {
-              return <img key={index} src={value} width="100px" />
-            }) }
-          </div>
-
-  <button onClick={this.generateText} className="button">Generate</button>
-</section>
-
-        { /* Results */ }
-        <section className="results">
-          { this.state.documents.map((value, index) => {
-            return (
-              <div key={index} className="results__result">
-                <div className="results__result__image">
-                  <img src={this.state.uploads[index]} width="250px" />
-                </div>
-                <div className="results__result__info">
-                  <div className="results__result__info__codes">
-                    <small><strong>Confidence Score:</strong> {value.confidence}</small>
-                  </div>
-                  <div className="results__result__info__codes">
-                    <small><strong>Pattern Output:</strong> {value.pattern.map((pattern) => { return pattern + ', ' })}</small>
-                  </div>
-                  <div className="results__result__info__text">
-                    <small><strong>Full Output:</strong> {value.text}</small>
-                  </div>
-                </div>
-              </div>
-            )
-          }) }
-        </section>
-      </div>
-    )
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={ComparisonList}/>
+          <Route path="/new" component={AddNewComparison}/>
+        </div>
+      </BrowserRouter>
+    );
   }
-
 }
 
+function PerformOCR(url) {
+  const worker = createWorker({
+    logger: m => console.log(m),
+  });
+  const doOCR = async () => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } = await worker.recognize(url);
+    setOcr(text);
+  };
+  const [ocr, setOcr] = useState('Recognizing...');
+  useEffect(() => {
+    doOCR();
+  });
+  return (
+    <p>{ocr}</p>
+  );
+}
 export default App;
