48ec691e810a007efbc3b213d30d344f3fe13395
jenleng <jen.leng.2019@smu.sis.edu.sg>
2021-04-06 13:57:18 +0800

    List of Products

--- hokohoko/src/components/Product.js
+++ hokohoko/src/components/Product.js
@@ -1,28 +1,21 @@
 import { React, Component } from 'react';
 import {Accordion, Card} from 'react-bootstrap';
 
-
-
 class Product extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          id: 0,
-          name: ''
-        };
-      }
-
+  constructor(props) {
+    super(props)
+}
     render() {
         return (
             <div>
               <Card>
-                <Accordion.Toggle as={Card.Header} eventKey={this.state.id.toString()}>
-                {this.state.name}
+                <Accordion.Toggle as={Card.Header} eventKey={this.props.id.toString()}>
+                {this.props.name}
               </Accordion.Toggle>
-                <Accordion.Collapse eventKey={this.state.id.toString()}>
+                <Accordion.Collapse eventKey={this.props.id.toString()}>
                   <Card.Body>
 
-                    <Card.Text>{this.state.name}</Card.Text>
+                    <Card.Text>{this.props.description}</Card.Text>
 
                   </Card.Body>
                 </Accordion.Collapse>
--- hokohoko/src/components/ProductList.js
+++ hokohoko/src/components/ProductList.js
@@ -2,15 +2,23 @@ import { React, Component } from 'react';
 import Product from './Product';
 
 // No. of products = No. of cards in accordion
-let products = [
-    {id:0, name:"A"} 
+const products = [
+    { id: 0, name: "A", description:"First" },
+    { id: 1, name: "B", description:"Second" }
 ]
 
 const productList = products.map((product) =>
-<div>
-<Product />
-</div>
+    <div>
+        <Product key={product.id}
+            id={product.id}
+            name={product.name}
+            description={product.description}
+        />
+    </div>
 );
+
+
+
 class ProductList extends Component {
     render() {
         return (
