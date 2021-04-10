b38f5a643886e70e25d9bdc95762aa70ffb1b062
meganseah <49828224+meganseah@users.noreply.github.com>
2021-04-10 17:19:23 +0800

    sort

--- hokohoko/src/components/ProductList.js
+++ hokohoko/src/components/ProductList.js
@@ -3,17 +3,17 @@ import Product from './Product';
 
 // No. of products = No. of cards in accordion
 let products = [
-    { id: 0, name: "A", description: "First", platforms: [{name: "Lazada", end_price: 50}, {name: "Grab", end_price: 20}] },
+    { id: 0, name: "A", description: "First", platforms: [{name: "Lazada", end_price: 50}, {name: "Grab", end_price: 20}, {name: "Laz", end_price: 100}] },
     { id: 1, name: "B", description: "Second", platforms: [{name: "Shoppee", end_price: 7}] }
 ]
 
-const productList = products.map((product) =>
+var productList = products.map((product) =>
     <div>
         <Product key={product.id}
             id={product.id}
             name={product.name}
             description={product.description}
-            platforms={product.platforms}
+            platforms={product.platforms.sort((a, b) => (a.end_price > b.end_price) ? 1 : -1)}
         />
     </div>
 );
