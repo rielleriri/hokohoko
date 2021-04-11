dd377cdb5508199834fe9c1aebe49caea1d3520e
jenleng <jen.leng.2019@smu.sis.edu.sg>
2021-04-11 22:03:35 +0800

    EditItem Page in Bootstrap

--- hokohoko/src/components/EditItem.js
+++ hokohoko/src/components/EditItem.js
@@ -1,33 +1,64 @@
-import React, { Component } from 'react';
+// React
+import { React, Component } from 'react';
 import { Link } from 'react-router-dom';
+// Others
+import { Button, Container, Form, Row, Col } from 'react-bootstrap';
+// Components
+
+// CSS
+import '../styles.css';
 
 class EditItem extends Component {
-  constructor(props) {
-    super(props);
-    this.onEdit = this.onEdit.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.onEdit = this.onEdit.bind(this);
+    }
 
-  onEdit(e) {
-    this.props.location.onEdit(e);
-    this.props.history.push('/');
-  }
+    onEdit(e) {
+        this.props.location.onEdit(e);
+        this.props.history.push('/');
+    }
 
-  render() {
-    return (
-      <div className='form-container'>
-        <form className='form-child-big' onSubmit={this.onEdit}>
-          <input
-            maxLength='25'
-            placeholder='Type name here'
-            defaultValue={this.props.location.name}
-          />
-          <br />
-          <button>Update</button>
-        </form>
-        <Link className='form-child-small back-link' to='/'>Back</Link>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Container fluid>
+
+                <Row>
+                    <Col>
+                        <h3>Edit Product Name</h3>
+                    </Col>
+                    <Col>
+                    <Link className='form-child-small back-link' to='/'>
+                        <Button className="float-right" variant="primary">
+                        Back
+                        </Button>
+                        </Link>
+                    </Col>
+                </Row>
+                <hr />
+                
+                <Form className='form-child-big' onSubmit={this.onEdit}>
+                <Row>
+                    <Col xs={4}>
+                    <Form.Group>
+                        <Form.Control
+                            maxLength='25'
+                            placeholder='Type name here'
+                            defaultValue={this.props.location.name}
+                        />
+                    </Form.Group>
+                    </Col>
+                    <Col>
+                    <Form.Group>
+                        <Button type="submit" variant="success">Update</Button>
+                    </Form.Group>
+                    </Col>
+                </Row>
+                </Form>
+
+            </Container>
+        );
+    }
 }
 
 export default EditItem;
